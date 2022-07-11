00145db58cf1370481a2cdd4813572e6604fa680
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-07-11 09:22:30 +0700

    1
    1

--- day4/find.js
+++ day4/find.js
@@ -3,10 +3,10 @@ var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("Databases");
-  dbo.collection("table_1").find({}).toArray(function(err, result) {
+  var dbo = db.db("box1");
+  dbo.collection("table").find({}).toArray(function(err, result) {
     if (err) throw err;
-    console.log(result[0].name);
+    console.log(result);
     db.close();
   });
 });
